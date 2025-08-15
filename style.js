d231ff8cd41a1fa0bbfe5b83d5d527e48cbed1fe
Karasamir2013 <karasfaragalla620@gmail.com>
2025-08-15 21:19:47 +0400

    Update style.js

--- style.js
+++ style.js
@@ -1,19 +1,10 @@
-body {
-    text-align: center;
-    background-color: lightblue;
-    font-family: Arial, sans-serif;
-}
-
-canvas {
-    border: 2px solid black;
-    background-color: #eee;
-}  
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 let player = { x: 100, y: 100, size: 20, speed: 4, inCar: false };
 let car = { x: 300, y: 260, w: 40, h: 20, speed: 6 };
 let keys = {};
+let canEnter = true;
 
 let buildings = [
     { x: 200, y: 100, w: 100, h: 100 },
@@ -35,10 +26,10 @@ function movePlayer() {
     let oldX = player.x;
     let oldY = player.y;
 
-    if (keys["ArrowUp"]) player.y -= player.speed;
-    if (keys["ArrowDown"]) player.y += player.speed;
-    if (keys["ArrowLeft"]) player.x -= player.speed;
-    if (keys["ArrowRight"]) player.x += player.speed;
+    if (keys["ArrowUp"] && player.y > 0) player.y -= player.speed;
+    if (keys["ArrowDown"] && player.y + player.size < canvas.height) player.y += player.speed;
+    if (keys["ArrowLeft"] && player.x > 0) player.x -= player.speed;
+    if (keys["ArrowRight"] && player.x + player.size < canvas.width) player.x += player.speed;
 
     // Collision with buildings
     for (let b of buildings) {
@@ -49,22 +40,26 @@ function movePlayer() {
     }
 
     // Enter car
-    if (keys["e"] && checkCollision(player, {x: car.x, y: car.y, w: car.w, h: car.h})) {
+    if (keys["e"] && checkCollision(player, {x: car.x, y: car.y, w: car.w, h: car.h}) && canEnter) {
         player.inCar = true;
+        canEnter = false;
+        setTimeout(() => canEnter = true, 300);
     }
 }
 
 function moveCar() {
-    if (keys["ArrowUp"]) car.y -= car.speed;
-    if (keys["ArrowDown"]) car.y += car.speed;
-    if (keys["ArrowLeft"]) car.x -= car.speed;
-    if (keys["ArrowRight"]) car.x += car.speed;
+    if (keys["ArrowUp"] && car.y > 0) car.y -= car.speed;
+    if (keys["ArrowDown"] && car.y + car.h < canvas.height) car.y += car.speed;
+    if (keys["ArrowLeft"] && car.x > 0) car.x -= car.speed;
+    if (keys["ArrowRight"] && car.x + car.w < canvas.width) car.x += car.speed;
 
     // Exit car
-    if (keys["q"]) {
+    if (keys["q"] && canEnter) {
         player.inCar = false;
-        player.x = car.x + 50;
+        player.x = car.x + car.w + 10;
         player.y = car.y;
+        canEnter = false;
+        setTimeout(() => canEnter = true, 300);
     }
 }
 
@@ -77,8 +72,8 @@ function drawMap() {
         ctx.fillRect(b.x, b.y, b.w, b.h);
     }
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(car.x, car.y, car.w, car.h); // car
+    ctx.fillStyle = "green"; // car
+    ctx.fillRect(car.x, car.y, car.w, car.h);
 }
 
 function drawPlayer() {
@@ -89,12 +84,14 @@ function drawPlayer() {
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawMap();
+
     if (player.inCar) {
         moveCar();
     } else {
         movePlayer();
         drawPlayer();
     }
+
     requestAnimationFrame(gameLoop);
 }
 
